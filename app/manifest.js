4667af1febc8285663a43f8b1167a4ea49604dff
Shumail Abdulrehman <47891@students.riphah.edu.pk>
2026-05-16 14:49:38 +0500

    add web app manifest route

---
+++ app/manifest.js
@@ -0,0 +1,11 @@
+export default function manifest() {
+  return {
+    name: "Abid Tour & Travels",
+    short_name: "Abid Tours",
+    description: "Northern Pakistan tours, travel packages, and vehicle rentals.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#f0f4fa",
+    theme_color: "#0f1f3d",
+  };
+}
