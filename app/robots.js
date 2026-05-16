73d2945cbd68199f39ba2871c733ea7af698afb2
Shumail Abdulrehman <47891@students.riphah.edu.pk>
2026-05-16 14:49:38 +0500

    add robots rules for crawlers

---
+++ app/robots.js
@@ -0,0 +1,38 @@
+import { siteSettings } from "../src/lib/site-content.js";
+
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+      {
+        userAgent: "Google-Extended",
+        allow: "/",
+      },
+      {
+        userAgent: "Applebot-Extended",
+        allow: "/",
+      },
+    ],
+    sitemap: `${siteSettings.siteUrl}/sitemap.xml`,
+    host: siteSettings.siteUrl,
+  };
+}
